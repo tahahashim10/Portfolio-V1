b04427e9e1e8619b9477c9cc147b25b9c5f833b2
Taha Hashim <tahahashim10@gmail.com>
2024-04-26 15:04:46 -0400

    services section

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,12 +1,71 @@
 import React from 'react';
+import {BsArrowUpRight} from 'react-icons/bs';
+import {motion} from 'framer-motion';
+import {fadeIn} from '../variants';
+
+const services = [ 
+  {
+    name: 'UI/UX Design', 
+    descrition: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, numquam. Harum deleniti id sapiente.', 
+    link: 'Learn more',
+  }, 
+  {
+    name: 'Development', 
+    descrition: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, numquam. Harum deleniti id sapiente.', 
+    link: 'Learn more',
+  }, 
+  {
+    name: 'Digital Marketing', 
+    descrition: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, numquam. Harum deleniti id sapiente.', 
+    link: 'Learn more',
+  }, 
+  {
+    name: 'Product Branding', 
+    descrition: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, numquam. Harum deleniti id sapiente.', 
+    link: 'Learn more',
+  },
+
+
+];
 
 const Services = () => {
   return (
 
-    <div className='section' id='services'>
-      Services
-    
-    </div>
+    <section className='section' id='services'>
+      <div className='container mx-auto'>
+        <div className='flex flex-col lg:flex-row'>
+          <div className='flex-1 lg:bg-services lg:bg-bottom bg:no-repeat mx-blend-lighten mb-12 lg:mb-0'>
+            <h2 className='h2 text-accent mb-6'>What I Do.</h2>
+            <h3 className='h3 max-w-[455px] mb-16'>
+              I'm a Computer Science student at the Univeristy of Toronto.
+            </h3>
+            <button className='btn btn-sm'>See my work</button>
+          </div>
+
+          <div className='flex-1'>
+            <div>
+              {services.map((service, index) => {
+                const {name, description, link} = service;
+                return (
+                  <div className='border-b border-white/20 h-[146px] mb-[38px] flex' key={index}>
+                    <div className='max-w-[476px]'>
+                      <h4 className='text-[20px] tracking-wider font-primary font-semibold mb-6'>{name}</h4>
+                      <p className='font-secondary leading-tight'>{description}</p>
+                    </div>
+                    <div className='flex flex-col flex-1 items-end'>
+                      <a href='#' className='btn w-9 h-9 mb-[42px]'>
+                        <BsArrowUpRight />
+                      </a>
+                      <a href='#'>{link}</a>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
 
 
   );
