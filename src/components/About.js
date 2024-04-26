b72c67a9ec5fdc83d843909cdca0261d292b2b3a
Taha Hashim <tahahashim10@gmail.com>
2024-04-26 02:31:46 -0400

    done most of about me

--- src/components/About.js
+++ src/components/About.js
@@ -9,12 +9,14 @@ const About = () => {
   return (
     <section className='section' id='about' ref={ref}>
       <div className='container mx-auto'>
-        <div>
-          <div className='flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top'>img</div>
-          <div>
-            <h2>About me.</h2>
-            <h3>I'm a Computer Science student at the Univeristy of Toronto.</h3>
-            <p>
+        <div className='flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen'>
+          <motion.div variants={fadeIn('right', 0.3)} initial="hidden" whileInView={'show'} viewport={{once: false, amount: 0.3}} className='flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top'></motion.div>
+          <motion.div variants={fadeIn('left', 0.5)} initial="hidden" whileInView={'show'} viewport={{once: false, amount: 0.3}} className='flex-1'>
+            <h2 className='h2 text-accent'>About me.</h2>
+            <h3 className='h3 mb-4'>
+              I'm a Computer Science student at the Univeristy of Toronto.
+            </h3>
+            <p className='mb-6'>
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Unde, 
               numquam. Harum deleniti id sapiente.
@@ -28,18 +30,26 @@ const About = () => {
               </div>
               <div>
                 <div className='text-[40px] font-tertiary text-gradient mb-2'>
-                  { inView ? <CountUp start={0} end={5} duration={3} /> : null}
+                  { inView ? <CountUp start={0} end={10} duration={3} /> : null}
+                  k+
                 </div>
-                <div className='font-primary text-sm tracking-[2px]'>Years of <br/>Experience</div>
+                <div className='font-primary text-sm tracking-[2px]'>Projects <br/>Completed</div>
               </div>
               <div>
                 <div className='text-[40px] font-tertiary text-gradient mb-2'>
-                  { inView ? <CountUp start={0} end={5} duration={3} /> : null}
+                  { inView ? <CountUp start={0} end={12} duration={3} /> : null}
+                  k+
                 </div>
-                <div className='font-primary text-sm tracking-[2px]'>Years of <br/>Experience</div>
+                <div className='font-primary text-sm tracking-[2px]'>Satisfied  <br/>Clients</div>
               </div>
             </div>
-          </div>
+            <div className='flex gap-x-8 items-center'>
+              <button className='btn btn-lg'>Contact me</button>
+              <a href='#' className='text-gradient btn-link'>
+                My Portfolio
+              </a>
+            </div>
+          </motion.div>
         </div>
       </div>
     </section>
